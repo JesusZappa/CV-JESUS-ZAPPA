d472ee2d85fa36cae795328e368fd2e52a678726
Jesus-zappa <111644484+JesusZappa@users.noreply.github.com>
2023-12-01 12:48:17 -0500

    Transicion discolver paginas
    Transicion discolver paginas  v01

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+  document.querySelectorAll('a').forEach(function(link) {
+    link.addEventListener('click', function(e) {
+      e.preventDefault();
+      const href = this.getAttribute('href');
+      document.body.style.opacity = 0; // Establecer opacidad a 0
+      setTimeout(function() {
+        window.location.href = href; // Cambiar a la nueva página después de la transición
+      }, 200); // Tiempo de transición (0.5 segundos en este caso)
+    });
+  });
+});
